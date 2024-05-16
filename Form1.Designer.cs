b6ef79a49ca0583589ff3c08cba607105d51d46c
Gamze <101417422+Gamzekasyol@users.noreply.github.com>
2024-05-16 22:03:16 +0300

    Telefon Rehberi Uygulaması
    Bu uygulama ile rehberinize kişiler ekleyebilir, kişileri güncelleyebilir veya silebilirsiniz. Ayrıca Excele Aktar butonu ile rehberinizdeki kişileri bir excel dosyasına aktarabilirsiniz.

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace TelefonRehberiSistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtara = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.txttelefon = new System.Windows.Forms.TextBox();
+            this.btnkaydet = new System.Windows.Forms.Button();
+            this.btngüncelle = new System.Windows.Forms.Button();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btnara = new System.Windows.Forms.Button();
+            this.lblsay = new System.Windows.Forms.Label();
+            this.btnexcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtara
+            // 
+            this.txtara.Location = new System.Drawing.Point(34, 28);
+            this.txtara.Name = "txtara";
+            this.txtara.Size = new System.Drawing.Size(361, 22);
+            this.txtara.TabIndex = 0;
+            // 
+            // dgv
+            // 
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(43, 73);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(352, 249);
+            this.dgv.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(445, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(436, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(436, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Telefon";
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(506, 89);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(191, 22);
+            this.txtad.TabIndex = 5;
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Location = new System.Drawing.Point(506, 144);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(191, 22);
+            this.txtsoyad.TabIndex = 6;
+            // 
+            // txttelefon
+            // 
+            this.txttelefon.Location = new System.Drawing.Point(506, 198);
+            this.txttelefon.Name = "txttelefon";
+            this.txttelefon.Size = new System.Drawing.Size(191, 22);
+            this.txttelefon.TabIndex = 7;
+            // 
+            // btnkaydet
+            // 
+            this.btnkaydet.Location = new System.Drawing.Point(426, 257);
+            this.btnkaydet.Name = "btnkaydet";
+            this.btnkaydet.Size = new System.Drawing.Size(82, 43);
+            this.btnkaydet.TabIndex = 8;
+            this.btnkaydet.Text = "Kaydet";
+            this.btnkaydet.UseVisualStyleBackColor = true;
+            this.btnkaydet.Click += new System.EventHandler(this.btnkaydet_Click);
+            // 
+            // btngüncelle
+            // 
+            this.btngüncelle.Location = new System.Drawing.Point(530, 257);
+            this.btngüncelle.Name = "btngüncelle";
+            this.btngüncelle.Size = new System.Drawing.Size(86, 43);
+            this.btngüncelle.TabIndex = 9;
+            this.btngüncelle.Text = "Güncelle";
+            this.btngüncelle.UseVisualStyleBackColor = true;
+            this.btngüncelle.Click += new System.EventHandler(this.btngüncelle_Click);
+            // 
+            // btnsil
+            // 
+            this.btnsil.Location = new System.Drawing.Point(641, 257);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(81, 43);
+            this.btnsil.TabIndex = 10;
+            this.btnsil.Text = "Sil";
+            this.btnsil.UseVisualStyleBackColor = true;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // btnara
+            // 
+            this.btnara.Location = new System.Drawing.Point(416, 17);
+            this.btnara.Name = "btnara";
+            this.btnara.Size = new System.Drawing.Size(82, 45);
+            this.btnara.TabIndex = 11;
+            this.btnara.Text = "Ara";
+            this.btnara.UseVisualStyleBackColor = true;
+            this.btnara.Click += new System.EventHandler(this.btnara_Click);
+            // 
+            // lblsay
+            // 
+            this.lblsay.AutoSize = true;
+            this.lblsay.Location = new System.Drawing.Point(463, 336);
+            this.lblsay.Name = "lblsay";
+            this.lblsay.Size = new System.Drawing.Size(0, 16);
+            this.lblsay.TabIndex = 12;
+            // 
+            // btnexcel
+            // 
+            this.btnexcel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnexcel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnexcel.Location = new System.Drawing.Point(600, 14);
+            this.btnexcel.Name = "btnexcel";
+            this.btnexcel.Size = new System.Drawing.Size(97, 48);
+            this.btnexcel.TabIndex = 13;
+            this.btnexcel.Text = "Excele Aktar";
+            this.btnexcel.UseVisualStyleBackColor = false;
+            this.btnexcel.Click += new System.EventHandler(this.btnexcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnexcel);
+            this.Controls.Add(this.lblsay);
+            this.Controls.Add(this.btnara);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.btngüncelle);
+            this.Controls.Add(this.btnkaydet);
+            this.Controls.Add(this.txttelefon);
+            this.Controls.Add(this.txtsoyad);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.txtara);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtara;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.TextBox txtsoyad;
+        private System.Windows.Forms.TextBox txttelefon;
+        private System.Windows.Forms.Button btnkaydet;
+        private System.Windows.Forms.Button btngüncelle;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnara;
+        private System.Windows.Forms.Label lblsay;
+        private System.Windows.Forms.Button btnexcel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TelefonRehberiSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn;
+        SqlDataAdapter adapter;
+        SqlCommand cmd;
+        System.Data.DataTable tablo;
+
+        private bool AlanlarıKontrolEt()
+        {
+            if (txtad.Text == "" || txtsoyad.Text == "" || txttelefon.Text == "")
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurunuz!");
+                return false;
+            }
+            return true;
+        }
+
+        void KisiGetir()
+        {
+            conn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dbRehber;Integrated Security=True;");
+            adapter = new SqlDataAdapter("SELECT * FROM Kisiler", conn);
+            tablo = new System.Data.DataTable();
+            conn.Open();
+            adapter.Fill(tablo);
+            dgv.DataSource = tablo;
+            conn.Close();
+        }
+        void KisiSayısı()
+        {
+            lblsay.Text = "Telefon Rehberinde " + dgv.RowCount + " Kişi Var! ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KisiGetir();
+            KisiSayısı();
+        }
+
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+            if (!AlanlarıKontrolEt()) return ; // ünlemin amacı eğer  kullanıcı alanları doldurmadı ise çalıştır demektir. return ise kaydet butonuna bastıktan sonra boş alan eklenmemesi içindir. Eğer return yazmazsak çalıştırmaya devam eder ve boş alan ekler.
+
+            string sorgu = ("Insert into Kisiler(Ad, Soyad, Telefon) values (@Ad, @Soyad, @Telefon)");
+            cmd = new SqlCommand(sorgu, conn);
+            cmd.Parameters.AddWithValue("@Ad", txtad.Text);
+            cmd.Parameters.AddWithValue("@Soyad", txtsoyad.Text);
+            cmd.Parameters.AddWithValue("@Telefon", txttelefon.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            KisiGetir();
+            MessageBox.Show(txtad.Text + " "+ txtsoyad.Text + "Eklendi!");
+        }
+
+        private void btngüncelle_Click(object sender, EventArgs e)
+        {
+            string sorgu = ("update Kisiler set ad=@Ad, soyad=@soyad, Telefon=@Telefon Where Id=@id");
+            cmd = new SqlCommand(sorgu, conn);
+            cmd.Parameters.AddWithValue("@Ad", txtad.Text);
+            cmd.Parameters.AddWithValue("@Soyad", txtsoyad.Text);
+            cmd.Parameters.AddWithValue("@Telefon", txttelefon.Text);
+            cmd.Parameters.AddWithValue("@id", dgv.CurrentRow.Cells[0].Value);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();   
+            KisiGetir();
+            MessageBox.Show(txtad.Text + " " + txtsoyad.Text + "Güncellendi!");
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            string ad = dgv.CurrentRow.Cells[1].Value.ToString();
+            string soyad = dgv.CurrentRow.Cells[2].Value.ToString();    
+            string sorgu = "Delete Kisiler where Id=@id";
+            cmd = new SqlCommand(sorgu, conn);
+            cmd.Parameters.AddWithValue("@id", dgv.CurrentRow.Cells[0].Value);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            KisiGetir();
+            MessageBox.Show(ad+" "+soyad+" Adlı Kişi Silindi!");
+
+        }
+        private void dgvKisiler_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtad.Text= dgv.CurrentRow.Cells[1].Value.ToString();
+            txtsoyad.Text= dgv.CurrentRow.Cells[2].Value.ToString();
+            txttelefon.Text= dgv.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnara_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataView dv = tablo.DefaultView;
+                dv.RowFilter = "Ad Like'" + txtara.Text + "%'";
+                dgv.DataSource = dv;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Bulunamadı!");
+            }
+        }
+
+        private void btnexcel_Click(object sender, EventArgs e)
+        {
+            // Nesne Üretildi
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            // Excel Görünür Hale Getirme
+            excel.Visible = true;
+            // Excel de yeni bir çalışma sayfası açma.
+            Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+            // Başlangıç yerini seçme. Yani excelde birinci sayfadan başladığımzı söylüyoruz.
+            Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+            // Başlangıç Sütununu Seç
+            int StartCol = 1;
+            // Başlangıç Satırnı Seç 
+            int StartRow = 1;
+            // DataGridView sütun sayıları kadar dönü ekle
+            for (int j =0; j<dgv.ColumnCount; j++) // Sütun başlıklarını atma.
+            {
+                // Excel başlıklarını atma.
+                Range myRange = (Range)sheet1.Cells[StartRow, StartCol + j]; // sayfamın hücrelerine satır ve j kadar sütun ekle.
+                myRange.Value2 = dgv.Columns[j].HeaderText; // j den aldığım verileri değer olarak ekle.
+            }
+            StartRow++; // Colon sayısı bittiğinde satırları arttır.
+            // DataGridView sütun sayıları kadar dönü ekle
+            for (int i=0; i<dgv.RowCount; i++) // verileri yazdırdığımız bölüm
+            {
+                for (int j=0; j<dgv.Columns.Count; j++)
+                {
+                    Range myRange = (Range)sheet1.Cells[StartRow + i, StartRow + j];
+                    myRange.Value2 = dgv.Rows[i].Cells[j].Value;
+                    myRange.EntireColumn.AutoFit(); // Excelde alanların boyutunu otomatik oluşturur.
+                }
+            }
+
+        }
+    }
+}
